33aec922d285a12f7c522da84410805d7926159a
CristopherVanegas <cristopher.platzi@gmail.com>
2023-02-25 14:13:09 -0800

    Deleted two useless lines

--- DB_1/Third_Partial/Project_BS1/inserts/categorias_TO_detalle_productos.sql
+++ DB_1/Third_Partial/Project_BS1/inserts/categorias_TO_detalle_productos.sql
@@ -13,24 +13,11 @@ declare @contador int;
 set @contador = 1;
 while (@contador <= 100)
 begin
-	-- select @rand = FLOOR(RAND()*(@max-@min+1))+@min;
-	-- insert into detalle_productos(cod_categoria) values (@rand)
-	
-	-- UPDATE detalle_productos SET detalle_productos.descripcion = (concat('Product ', @contador))
-
-	insert into detalle_productos(descripcion) values (concat('Product ', @contador))
-
+	select @rand = FLOOR(RAND()*(@max-@min+1))+@min;
+	insert into detalle_productos(cod_categoria, descripcion) values (@rand, concat('Product ', @contador))
 	set @contador = @contador + 1;
 end
 
 ---------------------------------------------------------------------------------------
-
-alter table detalle_productos
-	drop column descripcion;
-
-
-alter table detalle_productos
-	add descripcion varchar(15);
-
 
 select * from detalle_productos;
