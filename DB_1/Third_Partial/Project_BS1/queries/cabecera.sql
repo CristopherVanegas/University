50e5330d180d862369a6bbb8cebe2d183ddcc2d2
CristopherVanegas <cristopher.platzi@gmail.com>
2023-02-26 18:55:16 -0800

    ADDED datetimes randomly and selected min and max year

--- DB_1/Third_Partial/Project_BS1/queries/cabecera.sql
+++ DB_1/Third_Partial/Project_BS1/queries/cabecera.sql
@@ -39,10 +39,25 @@ begin
 	ABS(CAST(NEWID() as binary(6)) % (select max(id) from cliente)) + (select min(id) from cliente),
 	ABS(CAST(NEWID() as binary(6)) % (select max(nFactura) from detalle_venta)) + (select min(nFactura) from detalle_venta)
 	);
-	
 	set @contador = @contador + 1;
 end
 
 ---------------------------------------------------------------------------------
+--  INSERTING DATETIME  ---------------------------------------------------------
+---------------------------------------------------------------------------------
+
+DECLARE @counter INT = 1;
+
+WHILE (@counter <= 1000)
+BEGIN
+	-- m - d - a
+	UPDATE cabecera SET fecha = dateadd(day, abs(cast(newid() as binary(6)) % 7300) + 1, '2004-01-01');
+
+    SET @counter = @counter + 1;
+END
+
+---------------------------------------------------------------------------------
 
 select * from cabecera;
+select min(year(fecha)) from cabecera;
+select max(year(fecha)) from cabecera;
