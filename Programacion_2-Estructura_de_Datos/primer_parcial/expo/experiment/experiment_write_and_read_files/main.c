ce53ade24b9a9b8ed1e813575e0a0ccb37e15669
CristopherVanegas <cristopher.platzi@gmail.com>
2023-03-14 12:51:06 -0700

    ADDED write file func

--- Programacion_2-Estructura_de_Datos/primer_parcial/expo/experiment/experiment_write_and_read_files/main.c
+++ Programacion_2-Estructura_de_Datos/primer_parcial/expo/experiment/experiment_write_and_read_files/main.c
@@ -1,8 +1,45 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int writeFile();
+
 int main()
 {
-    printf("Hello world!\n");
+    printf("File manipulation!\n");
+    switch(writeFile())
+    {
+    case 0:
+    {
+        printf("El archivo ha sido creado con exito!");
+    }
+    break;
+
+    case 1:
+    {
+        printf("Error al ejecutar la creacion del archivo!");
+    }
+    break;
+
+    }
+    printf("");
+
     return 0;
 }
+
+int writeFile()
+{
+    FILE *archivo;
+    archivo = fopen("archivo_1.txt", "w");
+
+    if (archivo == NULL)
+    {
+        printf("Error al abrir el archivo.\n");
+        return 1;
+    }
+
+    fprintf(archivo, "Esta es una cadena de caracteres.\n");
+    fclose(archivo);
+    return 0;
+}
+
+
