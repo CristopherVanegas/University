ffb5221bec89ca897398bbff78adc42dd9cb773c
CristopherVanegas <cristopher.platzi@gmail.com>
2023-03-20 13:15:19 -0700

    ADDED talleres, arch binarios

--- Programacion_2-Estructura_de_Datos/primer_parcial/Talleres/Taller_5/Final/Taller_5-ArchBinarios-Registros/main.c
+++ Programacion_2-Estructura_de_Datos/primer_parcial/Talleres/Taller_5/Final/Taller_5-ArchBinarios-Registros/main.c
@@ -3,15 +3,72 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void menu();
 void addRegistros();
 void leerRegistro();
+void specificData();
 
 int main()
 {
-    addRegistros();
-    // leerRegistro();
+    menu();
 
     return 0;
+}
+
+void menu()
+{
+    int opt;
+
+    do
+    {
+        do
+        {
+            system("cls");
+            printf("                    WELCOME USER\n\n");
+            printf(" [1] Read all data\n");
+            printf(" [2] Add more data\n");
+            printf(" [3] Show specific data\n");
+            printf(" [4] Exit\n");
+
+            printf("\n Please select one option: ");
+
+            // get the selected option
+            fflush(stdin);
+            scanf("%i", &opt);
+            fflush(stdin);
+
+        }
+        while ( opt > 3 && opt < 0);
+
+        // exit the program if 4 was selected
+        if (opt == 4)
+            break;
+
+        // enters the program
+        else
+        {
+            switch(opt)
+            {
+            case 1:
+            {
+                leerRegistro();
+            }
+            break;
+            case 2:
+            {
+                addRegistros();
+            }
+            break;
+            case 3:
+            {
+                specificData();
+            }
+            break;
+            }
+        }
+
+    }
+    while( opt != 4 );
 }
 
 struct Producto
@@ -90,12 +147,25 @@ void leerRegistro()
     if (f = fopen("archivo.dat", "rb") == NULL)
         printf("\n    Error, this file cannot be read or it does not exist!");
 
-    else {
-        // while (!feof(f)) {
-        fread(&P1, sizeof(P1), 1, f);
-        printf("%s : %s : %f\n", P1.codigo_inventario, P1.descripcion, P1.precio);
-        printf("\n Successfully read!");
+    else
+    {
+        // while (!feof(f))
+        // {
+            fread(&P1, sizeof(P1), 1, f);
+            printf("%s : %s : %f\n", &P1.codigo_inventario, &P1.descripcion, &P1.precio);
+            printf("\n Successfully read!");
         // }
     }
 
+    char opt;
+    printf("\n\n Press any key to continue ");
+    fflush(stdin);
+    scanf("%c", opt);
+}
+
+void specificData()
+{
+    /*
+        code...
+    */
 }
