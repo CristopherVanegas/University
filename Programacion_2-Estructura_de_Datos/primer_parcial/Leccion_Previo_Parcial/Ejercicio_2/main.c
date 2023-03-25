980d981a3d560e2bb2e1a42b9c7ce28a368f167c
CristopherVanegas <cristopher.platzi@gmail.com>
2023-03-25 15:21:40 -0500

    Ejercicio 2: Archivo de texto, agregar datos de factura

--- Programacion_2-Estructura_de_Datos/primer_parcial/Leccion_Previo_Parcial/Ejercicio_2/main.c
+++ Programacion_2-Estructura_de_Datos/primer_parcial/Leccion_Previo_Parcial/Ejercicio_2/main.c
@@ -10,7 +10,7 @@ typedef struct
     char cedula_identidad[10];
     char nombre_completo[50];
     int edad;
-} persona;
+} Persona;
 
 
 int main()
@@ -23,70 +23,49 @@ int main()
 void pedirDatos()
 {
     int n;
+    Persona p;
+
     printf(" Cuantas personas desea ingresar?? ");
     fflush(stdin);
     scanf("%d", &n);
 
-    FILE *f;
-    f = fopen("archivo.txt", "rb+");
-    if (f == NULL)
-        printf("Error al abrir!");
-    else
-    {
-        persona p;
 
-        for (int i = 0; i < n; i++)
+    // inicia el bucle
+    for (int i = 0; i < n; i++)
+    {
+        printf(" Ingrese la cedula: ");
+        fflush(stdin);
+        fgets(&p.cedula_identidad, 10, stdin);
+
+
+        printf(" Ingrese el nombre completo: ");
+        fflush(stdin);
+        fgets(&p.nombre_completo, 50, stdin);
+
+
+        printf(" Ingrese la edad: ");
+        fflush(stdin);
+        scanf("%d", &p.edad);
+
+        printf("\n %s %s %d\n", p.cedula_identidad, p.nombre_completo, p.edad);
+
+
+        /* ESCRIBIR DATOS EN .TXT */
+        FILE *f;
+        f = fopen("arch.txt", "a+t");
+
+        if (f == NULL) printf("\t Problema al abrir el archivo!");
+        else
         {
-            system("cls");
-            printf(">>> Datos persona %i\n", i + 1);
-            printf("  > Escriba la cedula de identidad: ");
-            fflush(stdin);
-            // gets(&p.cedula_identidad);
-            fgets(p.cedula_identidad, 10, stdin);
+            fprintf(f, "%s\n", p.cedula_identidad);
             // fputs(p.cedula_identidad, f);
 
-
-            printf("  > Escriba el nombre completo: ");
-            fflush(stdin);
-            // gets(&p.nombre_completo);
-            fgets(p.nombre_completo, 50, stdin);
-            // fputs(p.nombre_completo, f);
-
-
-            printf("  > Escriba la edad: ");
-            fflush(stdin);
-            scanf("%d", &p.edad);
-            // fputs(p.edad, f);
-
-            // fwrite(p.cedula_identidad, sizeof(p.cedula_identidad), 1, f);
-            // fwrite(p.nombre_completo, sizeof(p.nombre_completo), 1, f);
-            // fwrite(p.edad, sizeof(p.edad), 1, f);
-
-            // fprintf(f, "%s %s %d \n", p.cedula_identidad, p.nombre_completo, p.edad);
-            // fscanf (f, "%s %s %d ", &p.cedula_identidad, &p.nombre_completo, &p.edad);
-
-            //fputs(p.cedula_identidad, f);
-            //fputs(p.nombre_completo, f);
-            //fputs(p.edad, f);
-
-
-            fwrite(&p, sizeof(persona), 1, f);
+            // rewind(f);
+            // system("cls");
+            // fscanf(f, "%s", &p.cedula_identidad);
+            // fgets(p.cedula_identidad, f);
         }
 
-
-        // mostrar
-
-        persona l;
-        rewind(f);
-        for (int i = 0; i < n; i++)
-        {
-            fread(&l, sizeof(persona), 1, f);
-            while (!feof(f)) {
-                if (l.cedula_identidad != "")
-                    printf("%s %s %d\n", l.cedula_identidad, l.nombre_completo, l.edad);
-            }
-        }
+        fclose(f);
     }
-
-    fclose(f);
 }
