fcb0427bf80cdeec9aa4ea0128446a06f6fe4afc
CristopherVanegas <cristopher.platzi@gmail.com>
2023-03-25 15:00:50 -0500

    Codelab 4: Lista enlazada, continuar programando

---
+++ Programacion_2-Estructura_de_Datos/primer_parcial/CodeLabs/codelab_4/main.c
@@ -0,0 +1,18 @@
+
+//     Lista enlazada     //
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+struct Info {
+    int item1;
+    char *item2;
+    float item3;
+};
+
+int main()
+{
+    printf("Hello world!\n");
+    return 0;
+}
