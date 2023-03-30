1c2941ca4bab5cc8dafae291447a09eb96441357
CristopherVanegas <cristopher.platzi@gmail.com>
2023-03-30 07:50:42 -0500

    UPGRADED, now it supports phrases in mayus

--- Programacion_2-Estructura_de_Datos/segundo_parcial/Tareas/Tarea_4/Tarea_4-Es_Palindromo_Pilas_Colas/main.c
+++ Programacion_2-Estructura_de_Datos/segundo_parcial/Tareas/Tarea_4/Tarea_4-Es_Palindromo_Pilas_Colas/main.c
@@ -1,12 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
 
 char frase[100];
 char fraseSinEspacios[100];
 
 char pila[100];
 char cola[100];
+
+void generaFraseSinEspacios();
+void aMinusculas();
+void llenarPila();
+void llenarCola();
+int compruebaSiEsPalindromoV2();
 
 int main()
 {
@@ -17,27 +24,12 @@ int main()
     fflush(stdin);
     fgets(frase, 100, stdin);
 
-    // llena la frase sin espacios //
-    int j = 0;
-    for (int i = 0; i < strlen(frase); i++) {
-        if (frase[i] != ' ') {
-                fraseSinEspacios[j] = frase[i];
-                j++;
-        }
-    }
+    generaFraseSinEspacios();
+    aMinusculas();
 
-    // comprueba si es palindromo //
-    int d = strlen(fraseSinEspacios);   // dimension de la frase sin espacios
-    int flag = 1; // 1: true; 0: false;
-
-
-    /*   Resto d-1 y d-i-2 para no contar los fin de linea o '\0'   */
-    for (int i = 0; i < d-1; i++) {                                     // recorre el array como cola
-        if (fraseSinEspacios[i] != fraseSinEspacios[d - i - 2]) {       // recorre el array como pila
-                flag = 0;
-                break;
-        }
-    }
+    llenarPila();
+    llenarCola();
+    int flag = compruebaSiEsPalindromoV2();
 
 
     // comprueba flags //
@@ -47,9 +39,63 @@ int main()
     printf("\n\n");
     printf(" > Frase original      : %s", frase);
     printf(" > Frase sin espacios  : %s", fraseSinEspacios);
-    printf(" > Longitud de la frase contando el fin de linea o \'0\': %d", d);
+    printf(" > Longitud de la frase original contando el fin de linea o \'0\': %d", strlen(frase));
     printf("\n\n");
 
     return 0;
+}
+
+void generaFraseSinEspacios()
+{
+    // llena la frase sin espacios //
+    int j = 0;
+    for (int i = 0; i < strlen(frase); i++)
+    {
+        if (frase[i] != ' ')
+        {
+            fraseSinEspacios[j] = frase[i];
+            j++;
+        }
+    }
+}
+
+int compruebaSiEsPalindromoV2()
+{
+    // comprueba si es palindromo //
+    int d = strlen(fraseSinEspacios);   // dimension de la frase sin espacios
+    int flag = 1; // 1: true; 0: false;
+
+
+    /*   Resto d-1 y d-i-2 para no contar los fin de linea o '\0'   */
+    for (int i = 0; i < d-1; i++)                                       // recorre el array como cola
+    {
+        // El condicional verifica si la pila y la cola en las posiciones correspondientes son desiguales.
+        // En tan caso de que sean desiguales entonces la bandera sera negativa y detiene el bucle.
+        if (cola[i] != pila[d - i - 2])
+        {
+            flag = 0;
+            break;
+        }
+    }
+
+    return flag;
+}
+
+void aMinusculas()
+{
+    for (int i = 0; i < strlen(fraseSinEspacios); i++)
+    {
+        fraseSinEspacios[i] = tolower(fraseSinEspacios[i]);
+    }
+}
+
+void llenarPila()
+{
+    strcpy(pila, fraseSinEspacios);
+}
+
+void llenarCola()
+{
+    strcpy(cola, fraseSinEspacios);
 }
 
