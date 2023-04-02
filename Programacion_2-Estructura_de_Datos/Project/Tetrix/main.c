d3fcd0f15042179fe2371d6de1dc9a36a64575df
CristopherVanegas <cristopher.platzi@gmail.com>
2023-04-02 18:59:40 -0500

    Base project.

---
+++ Programacion_2-Estructura_de_Datos/Project/Tetrix/main.c
@@ -0,0 +1,17 @@
+//     PROYECTO - ESTRUCTURA DE DATOS     //
+//  Integrantes:                          //
+//          > Jose Munioz                 //
+//          > Kevin Santana               //
+//          > Sergio Balarezo             //
+//          > Yelena Urinia               //
+//          > Cristopher Vanegas          //
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("Hello world!\n");
+    return 0;
+}
