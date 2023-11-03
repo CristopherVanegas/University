01ab99ef94dfb4f5e6872d33de10bc2c9dcb95fd
Cristopher Vanegas <cristopher.platzi@gmail.com>
2023-11-03 17:58:28 -0500

    Changed colors and added translate animation for Winning or Losing message

--- Programming_4-AndroidStudio/Project/Hangman_Game_Java/app/src/main/java/com/example/hangman_game_java/MainActivity.java
+++ Programming_4-AndroidStudio/Project/Hangman_Game_Java/app/src/main/java/com/example/hangman_game_java/MainActivity.java
@@ -161,7 +161,6 @@ public class MainActivity extends AppCompatActivity {
         edtInput.addTextChangedListener(new TextWatcher() {
             @Override
             public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-
             }
             @Override
             public void onTextChanged(CharSequence s, int start, int before, int count) {
