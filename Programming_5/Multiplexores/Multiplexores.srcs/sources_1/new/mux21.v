7776271f028f597859c41fb62c248636a2305929
Cristopher Vanegas <cristopher.platzi@gmail.com>
2024-06-25 07:33:22 -0500

    MUX 4x1 + Signal

--- Programming_5/Multiplexores/Multiplexores.srcs/sources_1/new/mux21.v
+++ Programming_5/Multiplexores/Multiplexores.srcs/sources_1/new/mux21.v
@@ -1,19 +1,27 @@
+// MUX 4x1 + Signal
+
 `timescale 1ns / 1ps
 
 module mux21(
-    input i0,
-    input i1,
-    input sel,
+    input A,
+    input B,
+    input C,
+    input D,
+    input [1:0] sel,
     output out
     );
     
     reg temp;
     
     always@(*) begin
-        if (sel == 1'b1)
-            temp = i0;
-        else
-            temp = i1;
+        if (sel[1] == 1'b0 && sel[0] == 1'b0)
+            temp = A;
+        else if (sel[1] == 1'b0 && sel[0] == 1'b1)
+            temp = B;
+        else if (sel[1] == 1'b1 && sel[0] == 1'b0)
+            temp = C;
+        else if (sel[1] == 1'b1 && sel[0] == 1'b1)
+            temp = D;
     end
     assign out = temp;
 endmodule
